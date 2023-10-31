bc89f236c452ddd0a9dc97ce90831d84f9ef1b60
Anhelina Babych <115937394+angelina-babych@users.noreply.github.com>
2023-10-31 17:07:23 +0200

    Add files via upload

---
+++ GroupManager/Models/CalendarMonthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GroupManager.Models
+{
+    public class CalendarMonthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Calendar calendar=value as Calendar;
+           // calendar.Selec
+            //var date=calendar.
+            return "Super";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GroupManager/Models/CharacteristicModel.cs
@@ -0,0 +1,36 @@
+﻿using GroupManager.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupManager.Models
+{
+    public class CharacteristicModel
+    {
+        public Student Student { get; set; }
+        public bool IsGoodStudent { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get;set; }
+        public string Patronymic { get; set; }
+        public string StartStudyDate { get; set; }
+        public string Specialization { get; set; }
+        public string Course { get; set;}
+        public string Group { get; set; }
+        public List<string> StudentRecomendations { get; set; }=new List<string>();
+        public string PhysicalCharacteristic { get; set; }
+        public string Collective { get; set; }
+        public string Behavior { get; set; }
+        public string PoliceSituations { get; set; }
+        public string LawAndOrderViolations { get; set; }
+        public string AlchogolSituations { get; set; }
+        public string ReadyToArmy { get; set; }
+        public string Courses { get; set; }
+        public string Director { get; set; } = "Олександр ПIТЯКОВ";
+        public string Tutor { get; set; } = "Свiтлана ГРИЦЕНКО";
+
+    }
+}
