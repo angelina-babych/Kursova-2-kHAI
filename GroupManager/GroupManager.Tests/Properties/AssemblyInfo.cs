7af9fbace4db2520a9b16a9be34f3419be857016
Anhelina Babych <115937394+angelina-babych@users.noreply.github.com>
2023-10-31 17:16:12 +0200

    Add files via upload

---
+++ GroupManager/GroupManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GroupManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GroupManager.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0f88667-1d17-471b-8033-6bbe81190959")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GroupManager/GroupManager.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLogic.Repositories;
+using GroupManager.Core.Context;
+using GroupManager.Core.Model;
+using GroupManager.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace GroupManager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddStudent()
+        {
+            //GroupRepository studentsRepo = new GroupRepository(new ApplicationContext());
+            //Group group = new Group { Id = Guid.NewGuid(), Name = "ssasdadssasa" };
+            //studentsRepo.Add(group);
+            //var expected = false;
+
+            ////Act
+            //var actual = studentsRepo.Add(group);
+            //Task.Delay(2950).Wait();
+            ////Assert
+            //Assert.AreEqual(true, true);
+            //StudentRepository studentsRepo = new StudentRepository(new ApplicationContext());
+            //Student stud = new Student {Id=Guid.NewGuid(), GroupId = new Guid("F6B44AE0-6F7E-4582-932F-5740F6E08E39") };
+            //studentsRepo.Add(stud);
+
+            //var expected = false;
+
+            ////Act
+            //var actual = studentsRepo.Add(stud);
+
+            ////Assert
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
